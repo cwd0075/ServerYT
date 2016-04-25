14a995ab929a7e31c7724e1b4e7270aee078890f
Run Run Shaw <webpac@ad.cityu.edu.hk>
2016-04-25 19:39:55 +0800

    Created a country json, and country video list ok

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 'use strict';
 
 var result = require('./lib/services');
-result.getVideo(function(err, res, body){
-	if(err) {throw err;}
-	else {console.log(body);}
+var countries = require('./json/countries.json');
 
-});
+for (var i = 0; i < countries.length; i++) {
+	result.addSearchParams(countries[i].code);
+	result.getVideo(function(err, res, body){
+		if(err) {throw err;}
+		else {
+			
+			console.log(body);
+			}
+	});    
+    
+}
 
+
+
+//Todo:
+//1. get all video list ... done
+//2. load video list based on country ... done
+//3. create country list ... done
+//4. save to mongodb
+//5. build scheduler
 
 
--- express.test.js
+++ express.test.js
@@ -1,13 +1,29 @@
 'use strict';
 
-var superagent = require('superagent')
-var expect = require('expect.js')
+var superagent = require('superagent');
+var expect = require('expect.js');
 var result = require('./lib/services');
+var countries = require('./json/countries.json');
+
+describe('countries.json',function(){
+  it('should return Worldwide country code',function(){
+    var test = '';
+    for (var i = 0; i < countries.length; i++) {
+      if (countries[i].name =='Worldwide')
+      {
+        test = countries[i].code;
+        break;
+      }
+    }
+    expect(test).to.eql('US');
+  });
+
+});
 
 describe('services.js', function(){
-  
-  
-  it('should return video list', function(done){
+    
+  it('should return hk video list', function(done){
+    result.addSearchParams('HK');
     result.getVideo(function(err, res, body){
       expect(err).to.eql(null);
       expect(body).to.be.an('object');
