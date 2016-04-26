704e39bdefaf90d930140690b2a6a8a275f0a811
Run Run Shaw <webpac@ad.cityu.edu.hk>
2016-04-26 20:31:16 +0800

    Add winston logger.

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ var ytSearch = require('./lib/services');
 var countries = require('./json/countries.json');
 var db = require('mongoskin').db('mongodb://localhost:27017/youtube', {safe: true});
 var async = require('async');
+var winston = require('winston');
 
+winston.add(
+  winston.transports.File, {
+    filename: 'ServerYTWinston.log',
+    level: 'info',
+    json: true,
+    timestamp: true
+  }
+);
+
+winston.handleExceptions(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
 function saveYoutubeToMongo(name, callback){
 	ytSearch.addSearchParams(name.code);
 	ytSearch.getVideo(function(err, res, body){
@@ -17,7 +28,7 @@ function saveYoutubeToMongo(name, callback){
 			db.collection('vids').insert(body, function(e, success) {
 			    if (e) callback(e); 
 			    if (success){ 
-			    	console.log(name.name +' added!');
+			    	winston.info(name.name +' record added!');
 			    	callback();
 				}
 			});
@@ -28,20 +39,22 @@ function saveYoutubeToMongo(name, callback){
 function updateDB(){
 	async.each(countries, saveYoutubeToMongo, function (error) {
 	  if(error) {
-	    throw error;
+	    winston.error(error);
 	  } else {
-	  	console.log('Finished!');
+	  	winston.info('Finished all countries update');
 	  }
 	});
 }
 
+
+
 db.collection('vids').remove({}, function(e, success) {
     if (e){
-    	throw e;
+    	winston.error(e);
     } 
     else	
     {
-    	console.log('Removed all!');
+    	winston.info('Removed all old database records');
     	updateDB();
     }
 });
@@ -53,8 +66,8 @@ db.collection('vids').remove({}, function(e, success) {
 //2. load video list based on country ... done
 //3. create country list ... done
 //4. save to mongodb ... done
-//5. add mongodb test
-//6. Add log file
+//5. add mongodb test ... done
+//6. Add log file ... done
 //7. build scheduler
 
 
