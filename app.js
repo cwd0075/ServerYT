df2deff20d7a91a685e9ebb512fe2cf4011ad45f
Run Run Shaw <webpac@ad.cityu.edu.hk>
2016-04-27 21:32:42 +0800

    Add cron to schedule run at 30 min each hr

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 'use strict';
 
 var updatemongo = require('./lib/updatemongo');
+var CronJob = require('cron').CronJob;
 
-updatemongo.clearDB(function(e, success) {
-    if (e){
-    	updatemongo.winston.error(e);
-    } 
-    else	
-    {
-    	updatemongo.winston.info('Removed all old database records');
-    	updatemongo.updateDB(function(error){
-    		if(error) {
-			    updatemongo.winston.error(error);
-			  } else {
-			  	updatemongo.winston.info('Finished all countries update');
-			  }
-    	});
-    }
-});
+function mainRun(){
+    updatemongo.clearDB(function(e, success) {
+        if (e){
+        	updatemongo.winston.error(e);
+        } 
+        else	
+        {
+        	updatemongo.winston.info('Removed all old database records');
+        	updatemongo.updateDB(function(error){
+        		if(error) {
+    			    updatemongo.winston.error(error);
+    			  } else {
+    			  	updatemongo.winston.info('Finished all countries update');
+    			  }
+        	});
+        }
+    });
     
-// var updateDB = function(callback){
-// 	async.each(countries, saveYoutubeToMongo, function (error) {
-// 	  if(error) {
-// 	    winston.error(error);
-// 	  } else {
-// 	  	winston.info('Finished all countries update');
-// 	  }
-// 	});
-// }
+}
 
+var job = new CronJob('00 30 * * * *', function() {
+        mainRun();
+    }, 
+null, false, 'Asia/Hong_Kong');
+
+job.start();
+
+//command to run in background: "nohup node app.js &"
 
 //Todo:
 //1. get all video list ... done
@@ -37,6 +39,6 @@ updatemongo.clearDB(function(e, success) {
 //4. save to mongodb ... done
 //5. add mongodb test ... done
 //6. Add log file ... done
-//7. build scheduler
+//7. build scheduler ... done
 
 
